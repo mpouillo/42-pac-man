3892a042e00b158f3f47bfecb18a53a81a08bd4f
Marceau Pouilloux <mpouillo@student.42lyon.fr>
2026-05-31 16:17:25 +0200

    changed score storage method and added save/load functions

--- src/highscore.py
+++ src/highscore.py
@@ -1,47 +1,58 @@
-from collections import defaultdict
-from typing import Dict, List
+from pathlib import Path
+from typing import List
+
+from pydantic import TypeAdapter
 
 from protocols import HighscoreEntry
 
 
 class HighscoreManager:
     def __init__(self) -> None:
-        self._scores: Dict[str, List[int]] = defaultdict(list)
+        self._scores: List[HighscoreEntry] = []
 
     def __str__(self) -> str:
         return str(self._scores)
 
     def add_entry(self, entry: HighscoreEntry) -> None:
         """Add an entry to highscores."""
-        self._scores[entry.name].append(entry.score)
+        self._scores.append(entry)
 
     def remove_entry(self, entry: HighscoreEntry) -> bool:
         """
         Attempt to remove an entry from highscores.
         Return False if entry is not in saved highscores, else True.
         """
-        if (
-            entry.name not in self._scores
-            or entry.score not in self._scores[entry.name]
-        ):
+        if entry not in self._scores:
             return False
 
-        idx = self._scores[entry.name].index(entry.score)
-        self._scores[entry.name].pop(idx)
-        if len(self._scores[entry.name]) == 0:
-            self._scores.pop(entry.name)
+        idx = self._scores.index(entry)
+        self._scores.pop(idx)
         return True
 
     def get_top_scores(self, amount: int) -> List[HighscoreEntry]:
         """Fetch and return top score HighscoreEntry."""
-        scores = dict(self._scores)
+        scores = self._scores[:]
         top_scores = []
         for _ in range(amount):
-            best_player = max(scores.keys(), key=lambda entry: max(entry))
-            top_score = max(scores[best_player])
-            top_scores.append(
-                HighscoreEntry(name=best_player, score=top_score)
-            )
-            scores[best_player].pop(top_score)
+            top_score = max(scores, key=lambda entry: entry.score)
+            top_scores.append(top_score)
+            idx = scores.index(top_score)
+            scores.pop(idx)
 
         return top_scores
+
+    def load_scores(self, file_path: Path) -> None:
+        if not file_path.exists() or file_path.is_dir():
+            raise ValueError("File does not exist.")
+
+        raw_data = file_path.read_bytes()
+        adapter = TypeAdapter(list[HighscoreEntry])
+        for entry in adapter.validate_json(raw_data):
+            self.add_entry(entry)
+
+    def save_scores(self, file_path: Path) -> None:
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+
+        adapter = TypeAdapter(list[HighscoreEntry])
+        json_bytes = adapter.dump_json(self._scores, indent=4)
+        file_path.write_bytes(json_bytes)

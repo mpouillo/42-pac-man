37b076d7643ef6843bc86fa7db96e1d891c7c6cb
Marceau Pouilloux <mpouillo@student.42lyon.fr>
2026-05-31 18:09:09 +0200

    fixed swapped coordinates + switched them all to (x, y) format

--- src/model/ghost.py
+++ src/model/ghost.py
@@ -23,8 +23,8 @@ class Ghost:
 
         self._speed: float = speed
         self._behavior_timer: float = 0.0
-        self._target_tile: Tuple[int, int] = (0, 0)     # (y, x)
-        self._checkpoint: Tuple[float, float] = (0, 0)  # (y, x)
+        self._target_tile: Tuple[int, int] = (0, 0)     # (x, y)
+        self._checkpoint: Tuple[float, float] = (0, 0)  # (x, y)
 
     @property
     def data(self) -> GhostData:
@@ -74,45 +74,46 @@ class Ghost:
     ) -> None:
         if self.state == GhostState.SCATTER and level.data:
             self._target_tile = (
-                int(numpy.random.randint(0, level.data.size_y)),
-                int(numpy.random.randint(0, level.data.size_x))
+                int(numpy.random.randint(0, level.data.size_x)),
+                int(numpy.random.randint(0, level.data.size_y))
             )
             return
 
         match self.type:
             case GhostType.PINK:
                 # Chase 2 cells ahead of Pacman's position
-                pacman_pos = numpy.array((int(pacman.y), int(pacman.x)))
+                pacman_pos = numpy.array((int(pacman.x), int(pacman.y)))
                 future_pos = numpy.array(pacman.direction.value) * 2
                 self._target_tile = tuple(numpy.add(pacman_pos, future_pos))
             case GhostType.RED:
                 # Chase Pacman's position
-                self._target_tile = (int(pacman.y), int(pacman.x))
+                self._target_tile = (int(pacman.x), int(pacman.y))
             case GhostType.ORANGE:
                 # Chase Pacman's position, but flee to spawn if within 8 cells
                 distance = abs(math.sqrt(
-                    (self.y - pacman.y) ** 2 + (self.x - pacman.x) ** 2
+                    (self.x - pacman.x) ** 2 + (self.y - pacman.y) ** 2
                 ))
                 if distance <= 8:
                     spawn = level.get_ghost_spawn(self.type)
-                    self._target_tile = (spawn.y, spawn.x)
+                    self._target_tile = (spawn.x, spawn.y)
+                else:
+                    self._target_tile = (int(pacman.x), int(pacman.y))
             case GhostType.BLUE:
                 # Corners Pacman relative to Blinky's position
                 red_pos = numpy.array(ghost_info[GhostType.RED]["position"])
                 pink_target = numpy.array(ghost_info[GhostType.PINK]["target"])
-                red_yx = numpy.array((red_pos[1], red_pos[0]))
-                difference = numpy.subtract(red_yx, pink_target)
+                difference = numpy.subtract(pink_target, red_pos)
                 self._target_tile = tuple(numpy.add(pink_target, difference))
             case _:
                 self._target_tile = (0, 0)
 
     def _move_towards_target(self, level: Level, delta_time: float) -> None:
         if self._checkpoint == (0.0, 0.0):
-            self._checkpoint = (float(int(self.y)), float(int(self.x)))
+            self._checkpoint = (float(int(self.x)), float(int(self.y)))
 
-        dy = self._checkpoint[0] - self.y
-        dx = self._checkpoint[1] - self.x
-        cp_distance = math.hypot(dy, dx)
+        dx = self._checkpoint[0] - self.x
+        dy = self._checkpoint[1] - self.y
+        cp_distance = math.hypot(dx, dy)
 
         move_distance = self._speed * delta_time
 
@@ -146,6 +147,10 @@ class Ghost:
         best_direction = self.direction
         min_distance = float('inf')
 
+        current_x = self._checkpoint[0]
+        current_y = self._checkpoint[1]
+        best_checkpoint = (current_x, current_y)
+
         for d in directions:
             if (
                 d == opposites[self.direction]
@@ -153,8 +158,8 @@ class Ghost:
             ):
                 continue
 
-            next_x = int(self._checkpoint[1]) + d.value[0]
-            next_y = int(self._checkpoint[0]) + d.value[1]
+            next_x = int(current_x) + d.value[0]
+            next_y = int(current_y) + d.value[1]
 
             if (
                 0 <= next_y < len(level.grid)
@@ -162,14 +167,14 @@ class Ghost:
                 and level.grid[next_y][next_x] != CellState.WALL
             ):
                 dist_sq = math.sqrt(
-                    (next_x - self._target_tile[1]) ** 2
-                    + (next_y - self._target_tile[0]) ** 2
+                    (next_x - self._target_tile[0]) ** 2
+                    + (next_y - self._target_tile[1]) ** 2
                 )
 
                 if dist_sq < min_distance:
                     min_distance = dist_sq
                     best_direction = d
-                    best_checkpoint = (float(next_y), float(next_x))
+                    best_checkpoint = (float(next_x), float(next_y))
 
         self.direction = best_direction
         self._checkpoint = best_checkpoint

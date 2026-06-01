1ac3f07f5a405464cec9bd1480510528fe57dd38
Marceau Pouilloux <mpouillo@student.42lyon.fr>
2026-06-01 09:00:55 +0200

    updated config.py and reflected changes on __main__.py

--- src/config.py
+++ src/config.py
@@ -11,25 +11,24 @@ class ConfigData(BaseModel):
     points_per_ghost: int
 
 
-class Config:
-    @staticmethod
-    def load(file_path: Path) -> ConfigData:
-        if not file_path.exists() or file_path.is_dir():
-            raise ValueError("File does not exist.")
+def load_config(file_path: str) -> ConfigData:
+    path = Path(file_path)
+    if not path.exists() or path.is_dir():
+        raise ValueError("File does not exist.")
 
-        try:
-            raw_data = file_path.read_text()
-        except Exception as e:
-            raise IOError(f"Error while reading config file: {e}")
+    try:
+        raw_data = path.read_text()
+    except Exception as e:
+        raise IOError(f"Error while reading config file: {e}")
 
-        try:
-            clean_data = "".join([
-                line for line in raw_data.split('\n')
-                if not line.strip().startswith('#')
-            ])
-            print(clean_data)
-            validated_data = ConfigData.model_validate_json(clean_data)
-        except Exception as e:
-            raise ValueError(f"Invalid data in config: {e}")
+    try:
+        clean_data = "".join([
+            line for line in raw_data.split('\n')
+            if not line.strip().startswith('#')
+        ])
+        print(clean_data)
+        validated_data = ConfigData.model_validate_json(clean_data)
+    except Exception as e:
+        raise ValueError(f"Invalid data in config: {e}")
 
-        return validated_data
+    return validated_data

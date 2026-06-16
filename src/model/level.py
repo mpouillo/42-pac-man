a1fec1ff27b2bcc0c1c821f6edbc2a2bfe6588d0
Zhanzhong Qian <zqian@z1r12p3.42lyon.fr>
2026-06-17 00:22:30 +0200

    move the username window when gameover

--- src/model/level.py
+++ src/model/level.py
@@ -130,8 +130,8 @@ class Level:
                 x = cx * 2 + 1
                 y = cy * 2 + 1
 
-                if not (cell == 0xF):
-                    grid[y][x] = CellState.PACGUM
+                grid[y][x] = CellState.PACGUM
+
                 if not (cell & TOP):
                     grid[y - 1][x] = CellState.PACGUM
                 if not (cell & RIGHT):

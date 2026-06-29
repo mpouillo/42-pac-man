114f8aef0812befb2b014c56a9e61d4cff39b1a4
Marceau Pouilloux <mpouillo@student.42lyon.fr>
2026-06-29 13:30:26 +0200

    fixed build not correctly resolving dependencies (assets, config)

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,21 @@
+
 import sys
+
+from pathlib import Path
 
 from src.config import load_config
 from src.controller.game_controller import GameController
 
 
 def main() -> None:
-    config = load_config("config.json")
+    if hasattr(sys, '_MEIPASS') or getattr(sys, 'frozen', False):
+        base_path = Path(sys.executable).parent
+    else:
+        base_path = Path(__file__).resolve().parent.parent
+
+    config_path = base_path / "config.json"
+    config = load_config(str(config_path))
+
     controller = GameController(config)
     controller.run()
 

7ffd264f0d1a91913781ada04aed617220cc532d
Marceau Pouilloux <mpouillo@student.42lyon.fr>
2026-06-29 11:51:28 +0200

    fixed pacgums appearing in 42 logo

--- src/model/level.py
+++ src/model/level.py
@@ -153,8 +153,8 @@ class Level:
                 x = cx * 2 + 1
                 y = cy * 2 + 1
 
-                grid[y][x] = CellState.PACGUM
-
+                if not (cell == 0xF):
+                    grid[y][x] = CellState.PACGUM
                 if not (cell & TOP):
                     grid[y - 1][x] = CellState.PACGUM
                 if not (cell & RIGHT):
